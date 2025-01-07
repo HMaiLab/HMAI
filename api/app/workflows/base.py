a94be2f86f394e01cd77fc5f088fcfae9a24c0bc
HMaiLab <shuaKhuka709@gmail.com>
2025-01-07 19:54:30 +0800

    Add embeddings model provider and refactor vector store classes for enhanced flexibility

--- api/app/workflows/base.py
+++ api/app/workflows/base.py
@@ -1,78 +1,70 @@
-import asyncio
+import logging
 from typing import Any, List
 
 from agentops.langchain_callback_handler import (
     AsyncCallbackHandler,
     LangchainCallbackHandler,
 )
+from langchain.output_parsers.json import SimpleJsonOutputParser
 
-from app.agents.base import AgentBase
-from app.utils.prisma import prisma
-from app.utils.streaming import CustomAsyncIteratorCallbackHandler
-from prisma.enums import AgentType
-from prisma.models import Workflow
+from app.agents.base import AgentFactory
+from app.utils.callbacks import CustomAsyncIteratorCallbackHandler
+
+logger = logging.getLogger(__name__)
 
 
 class WorkflowBase:
     def __init__(
         self,
-        workflow: Workflow,
+        workflow_steps: list[Any],
         callbacks: List[CustomAsyncIteratorCallbackHandler],
         session_id: str,
-        monitoring_callbacks: List[
+        constructor_callbacks: List[
             AsyncCallbackHandler | LangchainCallbackHandler
         ] = None,
         enable_streaming: bool = False,
     ):
-        self.workflow = workflow
+        self.workflow_steps = workflow_steps
         self.enable_streaming = enable_streaming
         self.session_id = session_id
-        self.monitoring_callbacks = monitoring_callbacks
+        self.constructor_callbacks = constructor_callbacks
         self.callbacks = callbacks
 
     async def arun(self, input: Any):
-        self.workflow.steps.sort(key=lambda x: x.order)
         previous_output = input
         steps_output = []
 
-        for stepIndex, step in enumerate(self.workflow.steps):
-            agent_config = await prisma.agent.find_unique_or_raise(
-                where={"id": step.agentId},
-                include={
-                    "llms": {"include": {"llm": True}},
-                    "datasources": {
-                        "include": {"datasource": {"include": {"vectorDb": True}}}
-                    },
-                    "tools": {"include": {"tool": True}},
+        for stepIndex, step in enumerate(self.workflow_steps):
+            agent_data = step["agent_data"]
+            input = previous_output
+            output_schema = step["output_schema"]
+            agent_base = AgentFactory(
+                enable_streaming=self.enable_streaming,
+                callbacks=self.constructor_callbacks,
+                session_id=self.session_id,
+                agent_data=agent_data,
+                output_schema=output_schema,
+            )
+
+            agent = await agent_base.get_agent()
+
+            agent_response = await agent.ainvoke(
+                input=input,
+                config={
+                    "callbacks": self.callbacks[stepIndex],
                 },
             )
-            agent = await AgentBase(
-                agent_id=step.agentId,
-                enable_streaming=True,
-                callbacks=self.monitoring_callbacks,
-                session_id=self.session_id,
-                agent_config=agent_config,
-            ).get_agent()
+            if output_schema:
+                # TODO: throw error if output is not valid
+                parser = SimpleJsonOutputParser()
+                try:
+                    agent_response["output"] = parser.parse(
+                        text=agent_response["output"]
+                    )
+                except Exception as e:
+                    logger.error(f"Error parsing output: {e}")
+                    agent_response["output"] = {}
 
-            agent_input = {
-                "input": previous_output,
-            }
-            if agent_config.type == AgentType.OPENAI_ASSISTANT:
-                agent_input = {
-                    "content": previous_output,
-                }
-
-            task = asyncio.ensure_future(
-                agent.ainvoke(
-                    input=agent_input,
-                    config={
-                        "callbacks": self.callbacks[stepIndex],
-                    },
-                )
-            )
-
-            await task
-            agent_response = task.result()
             previous_output = agent_response.get("output")
             steps_output.append(agent_response)
 
